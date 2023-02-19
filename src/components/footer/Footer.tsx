462a6b3a6aa46067dbca40fadc6091f94ee40470
Alex Richards <alexchristopherrichards@gmail.com>
2023-02-19 15:53:38 -0500

    Icon fix

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,24 +1,34 @@
 import React from 'react';
-import classNames from 'classnames';
-import { GitHub, Linkedin, Mail } from 'react-feather';
 import Link from 'next/link';
+import {
+  FaGithub as GitHub,
+  FaLinkedinIn as Linkedin,
+  FaEnvelope as Mail,
+} from 'react-icons/fa';
+import classNames from 'classnames';
 import styles from './Footer.module.scss';
 
 export default function Footer() {
   return (
     <div className={styles.footer}>
-      <Link href="https://github.com/acrichards3" target="_blank">
-        <GitHub className={classNames(styles.icon, styles.github)} size={40} />
-      </Link>
-      <Link href="https://www.linkedin.com/in/acrichards3/" target="_blank">
-        <Linkedin
-          className={classNames(styles.icon, styles.linkedin)}
-          size={40}
-        />
-      </Link>
-      <Link href="mailto:alexchristopherrichards@gmail.com">
-        <Mail className={classNames(styles.icon, styles.email)} size={40} />
-      </Link>
+      <p className={styles.text}>Let's Connect</p>
+      <div className={styles.iconArea}>
+        <div className={classNames(styles.iconContainer, styles.github)}>
+          <Link href="https://github.com/acrichards3" target="_blank">
+            <GitHub className={styles.icon} />
+          </Link>
+        </div>
+        <div className={classNames(styles.iconContainer, styles.linkedin)}>
+          <Link href="https://www.linkedin.com/in/acrichards3/" target="_blank">
+            <Linkedin className={styles.icon} />
+          </Link>
+        </div>
+        <div className={classNames(styles.iconContainer, styles.email)}>
+          <Link href="mailto:alexchristopherrichards@gmail.com">
+            <Mail className={styles.icon} />
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
